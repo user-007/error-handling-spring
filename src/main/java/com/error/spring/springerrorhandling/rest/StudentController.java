f92e8c4145b15b89906117baf13e07840bb47ff1
user-007 <wedncv777@gmail.com>
2021-03-12 08:32:41 +0200

    changed StudentController

--- src/main/java/com/error/spring/springerrorhandling/rest/StudentController.java
+++ src/main/java/com/error/spring/springerrorhandling/rest/StudentController.java
@@ -1,15 +1,23 @@
 package com.error.spring.springerrorhandling.rest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 @GetMapping("/student")
     public ResponseEntity<Student> getStudent(){
-        throw new ArrayIndexOutOfBoundsException();
+        //throw new StudentNotFoundException("Cannot find exception");
+    //returning the appropriate codes
+        return ResponseEntity.notFound().build();
     }
+/*@ResponseStatus(HttpStatus.NOT_FOUND)
+@ExceptionHandler({StudentNotFoundException.class})
+public ResponseEntity<Student> handleHelloException() {
 
+}*/
 }
